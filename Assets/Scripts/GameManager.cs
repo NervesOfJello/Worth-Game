140bca38991881f29d649b3ed2a87d04522bb904
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-10-26 12:15:28 -0500

    UI + Game Manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    //Serialized Fields
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+}
