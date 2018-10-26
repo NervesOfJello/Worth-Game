9177c9ba291e2ce6aaaa3c514b9b067531514c5d
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-10-26 12:52:44 -0500

    GameManager Updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
     //Serialized Fields
+    [SerializeField]
+    Slider TimerSlider;
+
+    [SerializeField]
+    int GameTime;
+
+    //variables
+    float maxTime;
+    float secondsPast;
+
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+        maxTime = GameTime;
+        StartCoroutine(GameTimer());
 	}
 	
 	// Update is called once per frame
@@ -17,5 +30,35 @@ public class GameManager : MonoBehaviour {
 	{
 		
 	}
+
+    //updates the fill of the progress bar
+    void UpdateTimeSlider()
+    {
+        if ((secondsPast / (maxTime - 1)) > 1)
+        {
+            TimerSlider.value = secondsPast / (maxTime - 1);
+        }
+        else
+        {
+            TimerSlider.value = 1;
+        }
+        
+    }
+
+    //Coroutines
+
+    //ticks down time left, increments seconds past, sets the time slider, and navigates to the end page when the timer runs out
+    IEnumerator GameTimer()
+    {
+        while (GameTime > 0)
+        {
+            GameTime--;
+            secondsPast++;
+            yield return new WaitForSeconds(1);
+            UpdateTimeSlider();
+            Debug.Log(GameTime + "seconds left");
+        }
+        SceneManager.LoadScene("EndScene");
+    }
 
 }
