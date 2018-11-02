3a9f489314b819ae494e0abfed641432e91e3e45
Henry Hoare <Henry.Hoare@iam.local>
2018-11-02 14:18:59 -0500

    Adds some art and gets roof turn off working

---
+++ Assets/Building.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+
+    SpriteRenderer spriteR;
+
+	// Use this for initialization
+	void Start () {
+        spriteR = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+
+            spriteR.enabled = false;
+        
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        spriteR.enabled = true;
+    }
+
+
+
+}
