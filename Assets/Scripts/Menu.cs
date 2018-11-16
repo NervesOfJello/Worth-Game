6615ee66994fa848db988de61b9c107da05f85e5
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-11-16 12:06:03 -0600

    Menu Added!

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    //editor-accessible field names
+    [SerializeField]
+    private string levelSceneName;
+    
+    //button methods
+    public void StartButtonClicked()
+    {
+        SceneManager.LoadScene(levelSceneName);
+    }
+
+    public void ExitButtonClicked()
+    {
+        Debug.Log("Exiting Application...");
+        Application.Quit();
+    }
+}
